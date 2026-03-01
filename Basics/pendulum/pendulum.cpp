c9abf72f41ca8f8d543d7010b4461ea25f0e87ea
Cesar <cesardelahaza@hotmail.es>
2026-03-01 22:14:38 +0100

    Start pendulum animation, close #1

--- Basics/pendulum/pendulum.cpp
+++ Basics/pendulum/pendulum.cpp
@@ -1,10 +1,33 @@
 #include <SFML/Graphics.hpp>
+#include <math.h>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Pendulum");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::White);
+
+    float pixels = 3779.5275591;
+    // Center of circunference of movement
+    float center_x = 400; // pixels
+    float center_y = 0; // pixels
+
+    
+    // Circle
+    sf::CircleShape circle(20);
+    circle.setFillColor(sf::Color::White);
+    circle.setPosition({center_x - 20, center_y - 20});
+    // Line
+    std::array line =
+        {
+            sf::Vertex{sf::Vector2f(center_x, 0)}, // the center of circunference
+            sf::Vertex{sf::Vector2f(center_x, center_y)}};
+    // Time
+    sf::Clock clock;
+
+    float gravity = 9.8 ;     // meters per second^2 
+    float lineLength = 0.1; // meters
+
+    float theta = M_PI / 4;
+    float omega = 0;
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -17,8 +40,22 @@ int main()
                 window.close();
         }
 
+        float dt = 0.00001; // clock.restart().asSeconds(); // delta time
+
+        // Dynamics
+        float alpha = -(gravity / lineLength) * std::sin(theta);
+        omega += alpha * dt;
+        theta += omega * dt;
+
+        circle.setPosition({400 - 20 + lineLength * pixels * std::sin(theta),
+                             - 20 + lineLength * pixels * std::cos(theta)});
+
+        line[1].position.x = 400 + lineLength * pixels * std::sin(theta);
+        line[1].position.y =   lineLength * pixels * std::cos(theta);
+
         window.clear();
-        window.draw(shape);
+        window.draw(line.data(), line.size(), sf::PrimitiveType::Lines);
+        window.draw(circle);
         window.display();
     }
 }
--- Diary.md
+++ Diary.md
@@ -51,6 +51,61 @@ There are a few helpful words:
 * resolve
 * resolves
 
+### Branches
+
+To change to any branch:
+
+```Bash
+# Pull the remote references without altering local branches
+git fetch origin
+
+# We switch to the local branch we want to
+git checkout -t origin/1-start-pendulum-animation
+```
+
+To delete any branch:
+
+```bash
+git branch -d name_of_the_undesired_branch
+```
+
+### Finish implementation (ticket) in branch and merging data
+
+1. Make sure your branch is updated
+    ```bash
+    git status
+    git add .
+    git commit -m "Name of implementation, close #n"
+    ```
+
+2. Go back to main
+    ```bash
+    git checkout main
+    git pull origin main
+    ```
+
+3. Merge your branch in main
+    ```bash
+    git merge name_of_branch
+    ```
+    If there are conflicts, resolve them. Then:
+    ```bash
+    git add .
+    git commit
+    ```
+
+4. Upload main to GitHub
+    ```bash
+    git push origin main
+    ````
+
+5. Optional. Delete the branch
+    ```bash
+    git branch -d name-of-branch
+    git push origin --delete name-of-branch
+    ```
+
+
 # SFML
 
 ### SFML Homebrew Setup
